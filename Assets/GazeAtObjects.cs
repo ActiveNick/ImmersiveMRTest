0eb405f99f37812b5b486177b0de5916221c8bc1
Nick Landry <nick@mobility42.com>
2017-10-26 16:47:33 -0400

    Initial code commit

---
+++ Assets/GazeAtObjects.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazeAtObjects : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangeColor(Color color)
+    {
+        Renderer r = gameObject.GetComponent<Renderer>();
+
+        r.material.color = color;
+    }
+}
